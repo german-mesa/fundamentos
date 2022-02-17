74ba4b1ec5f5636fcc59193625dae9615296d7a7
German Mesa <german.mesa@sap.com>
2022-02-17 16:34:54 +0100

    ejercicios

---
+++ ejercicios/Originales/listas.py
@@ -0,0 +1,41 @@
+
+def cuenta_vocales(palabra):
+    pass
+
+
+def main():
+    frase = 'Ahora el que no esta mal informado es porque no quiere'
+
+    # Convierte la frase en palabras
+    # CODE HERE -->
+    
+    # CODE HERE <-- 
+    print(f"La lista de palabras es {palabras}")
+
+    # Ordena lista de palabras
+    # CODE HERE -->
+    
+    # CODE HERE <-- 
+    print(f"La lista de palabras ordenada {palabras}")
+
+    # Ordena lista de palabras por su longitud
+    # CODE HERE -->
+    
+    # CODE HERE <-- 
+    print(f"La lista de palabras ordenada por longitud {palabras}")
+
+    # Ordena lista de palabras por número de vocales
+    # CODE HERE -->
+    
+    # CODE HERE <-- 
+    print(f"La lista de palabras ordenada por número de vocales {palabras}")
+
+    # Convierte la lista en minúsculas y ordena
+    # CODE HERE -->
+    
+    # CODE HERE <-- 
+    print(f"La lista de palabras en minúsculas ordenada {palabras}")
+
+
+if __name__ == '__main__':
+    main()
