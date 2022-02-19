59497e880a7a4226e3f647cb444af075cae4ffdb
German Mesa <german.mesa@sap.com>
2022-02-19 14:14:32 +0100

    commit

--- ejercicios/Originales/diccionarios.py
+++ ejercicios/Originales/diccionarios.py
@@ -1,32 +1,71 @@
 
 empleados = {
     'Alan Turing': { 
-        'age': 25, 
-        'salary': 1000 
+        'edad': 25, 
+        'salario': 1000 
     }
 }
 
+def edad_maxima():
+    maximo = 0
 
-def contratar_empleado(empleado):
-    empleados.update(empleado)
+    for _ , item in enumerate(empleados):
+        if empleados[item]['edad'] > maximo:
+            maximo = empleados[item]['edad']
 
-def despedir_empleado(empleado):
-    empleados.pop(empleado, None) 
+    return maximo
+
+def contar_edad():
+    grupo = {}
+
+    for _ , item in enumerate(empleados):
+        if empleados[item]['edad'] in grupo:
+            grupo[empleados[item]['edad']] +=1
+        else:
+            grupo[empleados[item]['edad']] =1
+
+    return grupo
+
+def ordenar_edad(reverse=False):
+    return {k: v for k, v in sorted(empleados.items(), key=lambda item: item[1]['salario'], reverse=reverse)}
+
 
 def main():
-    # Contrato unos cuantos empleado
-    contratar_empleado({'John Hopkins':{'age': 18, 'salary': 1000}})
-    contratar_empleado({'Sharon Lin':{'age': 30, 'salary': 8000}})
-    contratar_empleado({'Mikhail Tal':{'age': 40, 'salary': 15000}})
-    print(empleados)
+    # Vaciar el diccionario
+    empleados.clear()
 
-    # Sobreescribo un empleado
-    contratar_empleado({'John Hopkins':{'age': 20, 'salary': 1000}})
-    print(empleados)
+    # Añadir un registro a un diccionario
+    empleados.update({'Mikhail Tal':{'edad': 40, 'salario': 17000}})
+    empleados.update({'John Hopkins':{'edad': 18, 'salario': 1000}})
+    empleados.update({'Sharon Lin':{'edad': 30, 'salario': 8000}})
+    empleados.update({'Pichi Cual':{'edad': 40, 'salario': 15000}})
 
-    # Despedir un empleado
-    despedir_empleado('Sharon Lin')
-    print(empleados)
+    # Sobreescribo un registro de un diccionario
+    empleados.update({'John Hopkins':{'edad': 30}})
+
+    # Actualizar edad y salario
+    empleados['John Hopkins']['salario'] = 2000
+
+    # Eliminar un registro de un diccionario 
+    empleados.pop('Sharon Lin', None)
+ 
+    # Loop sobre un diccionario
+    for index, item in empleados.items():
+        print(f'{index} - {item}')
+
+    for index, item in enumerate(empleados):
+        print(f'{index} - {item}')
+
+    for item in empleados.items():
+        print(f'{item}')
+
+    # Algunas cosas que pueden hacerse con loops
+    print(f'La edad máxima de la plantilla es {edad_maxima()}')
+    print(f'Los grupos de edad son {contar_edad()}')
+
+    # Cosas que pueden hacerse con lambdas
+    print(f'El diccionario puede ordenarse por edad {ordenar_edad()}')
+    print(f'El diccionario puede ordenarse por edad inversa {ordenar_edad(reverse=True)}')
 
 if __name__ == '__main__':
     main()
